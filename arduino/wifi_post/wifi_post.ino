d64a075ccd4b990655c00751e5f13161dfe28c3f
Josep <josep.alos.pascual@hotmail.com>
2019-11-24 07:52:24 +0100

    Corrected everything

--- arduino/wifi_post/wifi_post.ino
+++ arduino/wifi_post/wifi_post.ino
@@ -1,25 +1,18 @@
-/*
-  Simple POST client for ArduinoHttpClient library
-  Connects to server once every five seconds, sends a POST request
-  and a request body
-  created 14 Feb 2016
-  modified 22 Jan 2019
-  by Tom Igoe
-  
-  this example is in the public domain
- */
+#include <SimpleDHT.h>
+
 #include <ArduinoHttpClient.h>
 #include <WiFi101.h>
+#include <SimpleDHT.h>
 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-/////// Wifi Settings ///////
+#define DHTPIN 2
+
+SimpleDHT11 dht11;
+
 char ssid[] = "svila";
 char pass[] = "12341234";
-
-char serverAddress[] = "192.168.0.3";  // server address
-int port = 8080;
-int statusCode = 0;
-String response;
+char serverAddress[] = "192.168.137.145";  // server address
+char post_url[] = "/temperatures/new_temperature";
+int port = 8000;
 
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, serverAddress, port);
@@ -27,41 +20,66 @@ int status = WL_IDLE_STATUS;
 
 void setup() {
   Serial.begin(9600);
-  while ( status != WL_CONNECTED) {
+  
+  while (status != WL_CONNECTED){
     Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);                   // print the network name (SSID);
+    Serial.println(ssid);
 
-    // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
   }
 
-  // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 }
 
-void loop() {
-  Serial.println("making POST request");
+String get_data_string(byte* temperatures){
+  char temps[25];
+  for(int i = 0; i < 12; i++){
+    sprintf(temps + (i*2), "%02x", temperatures[i]);
+    Serial.println(temps);
+  }
+  String postData = "data=" + String(temps);
+  
+  return postData;
+}
+
+void send_temperatures(byte* temperatures){
+  Serial.println("Making a POST request");
+  String postData = get_data_string(temperatures);
+
+  Serial.print("Sending data: ");
+  Serial.println(postData);
+  
   String contentType = "application/x-www-form-urlencoded";
-  String postData = "";
+  client.post(post_url, contentType, postData);
 
-  client.post("/temperatures/new_temperature", contentType, postData);
-  //client.get("/");
-
-  // read the status code and body of the response
-  statusCode = client.responseStatusCode();
-  response = client.responseBody();
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
 
   Serial.print("Status code: ");
   Serial.println(statusCode);
   Serial.print("Response: ");
   Serial.println(response);
+}
 
-  Serial.println("Wait five seconds");
-  delay(5000);
+void loop() {
+  byte temperatures[12];
+  for (int i = 0; i <  12; i++){
+    byte temperature = 0;
+    byte humidity = 0;
+    byte data[40] = {0};
+    if (dht11.read(DHTPIN, &temperature, &humidity, data)) {
+      Serial.println("Read DHT11 failed");
+    }else{
+      temperatures[i] = temperature;
+      Serial.print("Read temperature: ");
+      Serial.println(temperature);
+    }
+    delay(60 * 1000);
+  }
+  send_temperatures(temperatures);
 }
